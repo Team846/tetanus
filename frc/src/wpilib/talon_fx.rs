cecb339b9c75e189f52a68dbec42c1cba7d6c46c
kaustubh <kaustubh.khulbe@gmail.com>
2021-06-23 14:59:46 -0700

    added talon fx file, no work done though

---
+++ frc/src/wpilib/talon_fx.rs
@@ -0,0 +1,9 @@
+// // #include "ctre/phoenix/motorcontrol/can/BaseTalon.h"
+// // #include "ctre/phoenix/motorcontrol/IMotorControllerEnhanced.h"
+// // #include "ctre/phoenix/motorcontrol/MotorCommutation.h"
+// // #include "ctre/phoenix/motorcontrol/StatorCurrentLimitConfiguration.h"
+// // #include "ctre/phoenix/CustomParamConfiguration.h"
+// // #include "ctre/phoenix/sensors/AbsoluteSensorRange.h"
+// // #include "ctre/phoenix/sensors/SensorInitializationStrategy.h"
+
+use frc_sys::ctre::ctre_phoenix_motorcontrol_TalonFXControlMode;
